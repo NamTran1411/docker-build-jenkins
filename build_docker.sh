03605409de34aca455e265f3ed3df6db3259b8e3
Võ Nam Trân <102129266+NamTran1411@users.noreply.github.com>
2024-05-24 13:14:40 +0700

    update new 2

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Description: Script to increment Docker image version, build, and push the image to Docker Hub.
+# Description: Script to increment Docker image version, build, and push
 # Read the new version
 version=$(cat version.txt)
 ssh adminlc@192.168.64.2 "
