c2c77a5a04e6d369cc373f70ab9e3c8914962cd7
Võ Nam Trân <102129266+NamTran1411@users.noreply.github.com>
2024-06-04 16:36:34 +0700

    update code

--- build_docker.sh
+++ build_docker.sh
@@ -7,17 +7,6 @@ version_file="version.txt"
 if [[ -f "$version_file" ]]; then
   # Read the current version from the file
   current_version=$(cat "$version_file")
-else
-  # Create a new version.txt file with initial version 1.0.0
-  echo "1.0.0" > "$version_file"
-  current_version="1.0.0"
-fi
-
-echo "Current version: $current_version"
-
-# Check if version needs incrementing (skip if 1.0.0)
-if [[ "$current_version" != "1.0.0" ]]; then
-  # Tách version thành major, minor và patch
   IFS='.' read -r -a parts <<< "$current_version"
   major=${parts[0]}
   minor=${parts[1]}
@@ -26,12 +15,14 @@ if [[ "$current_version" != "1.0.0" ]]; then
   # Tạo version mới
   new_version="$major.$minor.$patch"
 else
+  # Create a new version.txt file with initial version 1.0.0
+  echo "1.0.0" > "$version_file"
+  current_version="1.0.0"
   echo "Version is already 1.0.0. Not incrementing."
-  new_version="$current_version"  # Keep version as 1.0.0
+  new_version="$current_version"
 fi
 
-
-
+echo "Current version: $current_version"
 # In version mới ra màn hình
 echo "Updated version to $new_version"
 
