86d817bc90b5053b7b5ad776388bed839ef9ee25
Võ Nam Trân <102129266+NamTran1411@users.noreply.github.com>
2024-06-04 13:28:48 +0700

    update dockerfile

--- build_docker.sh
+++ build_docker.sh
@@ -29,10 +29,13 @@ echo "$new_version" > "$version_file"
 
 # In version mới ra màn hình
 echo "Updated version to $new_version"
+
+
 version=$(cat version.txt)
+
 ssh adminlc@192.168.64.2 "
     cd ./Documents/docker-build-jenkins &&
     git pull origin main &&
-    docker compose build --build-arg VERSION=$version &&
-    docker tag docker_builder:latest docker_builder:$version
+    docker compose build --no-cache &&
+    docker tag docker_builder:$current_version docker_builder:$version && docker compose up -d
 "
