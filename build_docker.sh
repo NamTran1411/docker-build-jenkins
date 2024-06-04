3157795cb3088c7fbd1ad80b9f45b212e8977bb1
Võ Nam Trân <102129266+NamTran1411@users.noreply.github.com>
2024-06-04 14:16:29 +0700

    update dockerfile

--- build_docker.sh
+++ build_docker.sh
@@ -36,6 +36,6 @@ version=$(cat version.txt)
 ssh adminlc@192.168.64.2 "
     cd ./Documents/docker-build-jenkins &&
     git pull origin main &&
-    docker compose build --no-cache &&
-    docker tag docker_builder:$current_version docker_builder:$version && docker compose up -d
+    docker build -t docker_builder:$version . &&
+    docker compose up -d && docker image rm docker_builder:$current_version
 "
