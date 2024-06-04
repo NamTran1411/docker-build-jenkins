0613a73820de1d12bf1fc11b08ed191ffff71fef
Võ Nam Trân <102129266+NamTran1411@users.noreply.github.com>
2024-06-04 16:49:34 +0700

    update 8

--- build_docker.sh
+++ build_docker.sh
@@ -2,32 +2,34 @@
 
 # Description: Script to increment Docker image version, build, and push
 # Version file path
-version_file="version.txt"
-
-if [[ -f "$version_file" ]]; then
-  # Read the current version from the file
-  current_version=$(cat "$version_file")
-  IFS='.' read -r -a parts <<< "$current_version"
-  major=${parts[0]}
-  minor=${parts[1]}
-  patch=$((patch + 1))
-
-  # Tạo version mới
-  new_version="$major.$minor.$patch"
-else
-  current_version="1.0.0"
-  echo "Version is already 1.0.0. Not incrementing."
-  new_version="$current_version"
-fi
-
-echo "Current version: $current_version"
-# In version mới ra màn hình
-echo "Updated version to $new_version"
 
 # SSH execution with conditional docker image removal
 ssh adminlc@192.168.64.2 << EOF
-    cd ./Documents/docker-build-jenkins &&
-    git pull origin main &&
+    cd ./Documents/docker-build-jenkins
+
+    git pull origin main
+    version_file="version.txt"
+
+    if [[ -f $version_file ]]; then
+      # Read the current version from the file
+      current_version=$(cat $version_file)
+      IFS='.' read -r -a parts <<< $current_version
+      major=${parts[0]}
+      minor=${parts[1]}
+      patch=$((patch + 1))
+
+      # Tạo version mới
+      new_version="$major.$minor.$patch"
+    else
+      current_version="1.0.0"
+      echo "Version is already 1.0.0. Not incrementing."
+      new_version="$current_version"
+    fi
+
+    echo "Current version: $current_version"
+    # In version mới ra màn hình
+    echo "Updated version to $new_version"
+
     docker build -t docker_builder:$new_version . &&
     docker compose up -d
 
